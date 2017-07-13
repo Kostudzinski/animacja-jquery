a2217c42ac6c2f639a5df8150b9f8a04a5c1b825
Konrad Studzinski <kostudzinski@gmail.com>
2017-07-13 15:49:19 +0200

    popr

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 'use strict';
 
+
+
 $(document).ready(function () {
-    $('#button').click(function() {
-        $('#div').animate({width:'100px', height: '100px', marginLeft: '100px'}, 3000, function () {
-                        $('div').animate({backgroundColor: 'blue'}, 5000);   
-                        $('h2').html('Animacja ukończona').css({fontSize: '20px', color: 'white'}); }
-        
-          );
-        
-
-    });
-
-        
+    $('#button').click(function () {
+        $('#div').animate({
+                width: '100px',
+                height: '100px',
+                marginLeft: '100px'
+            }, 2000, colorChange(), setTimeout(napis , 5000))
+    })
 });
 
+function colorChange() {
+    $('#div').animate({
+        backgroundColor: 'blue'
+    }, 1000);
+}
+
+function napis() {
+        $('h2').html('Animacja ukończona').css({
+        fontSize: '20px',
+        color: 'white'
+    });
+}
